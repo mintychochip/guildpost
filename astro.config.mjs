06b3ca966e363b987f32d62c4527402f7241511c
mintychochip <jlo2@csub.edu>
2026-04-09 02:12:12 -0700

    fix: use @astrojs/cloudflare adapter for Pages SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
+  adapter: cloudflare(),
   // Deploying to root domain guildpost.tech
   build: {
     assets: 'assets',
